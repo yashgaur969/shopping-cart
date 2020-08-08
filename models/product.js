3fdae7f0a0e40ef646cb45d3685838fe57075b91
yashgaur969 <yashgaur969@gmail.com>
2020-08-08 20:46:52 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+
+
+
+const getProductsFromFile = (cb) => {
+    const p = path.join(
+        path.dirname(process.mainModule.filename),
+        'data',
+        'products.json'
+      );
+      fs.readFile(p, (err, fileContent) => {
+        if (err) {
+          return cb([]);
+        }
+        cb(JSON.parse(fileContent));
+      });
+    
+}
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price; 
+  }
+
+  save() {
+    const p = path.join(
+        path.dirname(process.mainModule.filename),
+        'data',
+        'products.json'
+      );
+      this.id = Math.random().toString();
+    getProductsFromFile(products => {
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
+    });
+  }
+
+  static fetchAll(cb) {
+      getProductsFromFile(cb)
+  }
+
+  static findById(id,cb){
+    getProductsFromFile(products => {
+        const product = products.find(p => p.id === id);
+        if(product!=undefined){
+          cb(product);
+        }
+      })
+  }
+};
